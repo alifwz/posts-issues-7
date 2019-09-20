4dd5f54fc977f44d40a9f83e9c4febd7c6118161
alifwz <54393000+alifwz@users.noreply.github.com>
2019-09-20 11:36:27 +0300

    Add files via upload

--- deletecollection.php
+++ deletecollection.php
@@ -1,18 +1,27 @@
 <?php
 include 'connection.php';
+session_start();
 
+
+if (!isset($_SESSION['SESS_MEMBER_ID']) || empty($_SESSION['SESS_MEMBER_ID'])) {
+	echo "<script>window.location='index.php?status=failed'</script>";
+}
+$uid = 0;
+if (isset($_SESSION['SESS_MEMBER_ID'][0]) and !empty($_SESSION['SESS_MEMBER_ID'][0])) {
+	$uid = $_SESSION['SESS_MEMBER_ID'][0];
+}
 $id=$_POST['id'];
 $idd = $_REQUEST['id'];
 $type = $_REQUEST['type'];
 
-if($type == 'uploads'){	
-	$query=mysql_query("DELETE FROM freelancer_mmv_userimages WHERE id='".$id."'",$con);	
+if($type == 'uploads'){
+	$query=mysql_query("DELETE FROM freelancer_mmv_userimages WHERE id='{$id}' and userid='{$uid}' ",$con);
 } else if($type == 'delpost'){
-	$query=mysql_query("DELETE FROM freelancer_mmv_userimages WHERE id='".$idd."'",$con);
-	echo "<script>window.location='index.php?status=delsuccess'</script>";
-} 
+	$query=mysql_query("DELETE FROM freelancer_mmv_userimages WHERE id='{$idd}' and userid='{$uid}' ",$con);
+	echo "<script>window.location='index.php?status=success'</script>";
+}
 
 if($query){
-	echo "done";	
+	echo "done";
 }
 ?>
